01e3f8e1bd38722d2f26217a59aba42c7b1851b2
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-10-21 15:05:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 disneyTheme_song = "";
 harryPotter_song = "";
 
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
 function preload() {
     disneyTheme_song = loadSound("disney_theme_song.mp3");
     harryPotter_song = loadSound("harry_potter_song.mp3");
@@ -12,8 +18,33 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded");
 }
 
 function draw() {
     image(video, 0, 0, 600, 400);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("Left Wrist X= ", leftWristX);
+
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist Y= ", leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Right Wrist X= ", rightWristX);
+
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist Y= ", rightWristY);
+    }
 }
