3046d30cd109e9217c1f2ce1b715d549c98eba63
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-11-01 10:28:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ function draw() {
     fill("#ff0000");
     stroke("#6e2bc8");
 
+    //Code for leftWrist
     disneyTheme_status = disneyTheme_song.isPlaying();
 
     if (score_leftWrist > 0.2) {
@@ -51,6 +52,21 @@ function draw() {
             document.getElementById("song_name").innerHTML = "Playing Disney Theme Song";
         }
     }
+
+    //Code for rightWrist
+    harryPotter_status = harryPotter_song.isPlaying();
+
+    if (score_rightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+
+        disneyTheme_song.stop()
+
+        if (harryPotter_status == false) {
+            harryPotter_song.play();
+
+            document.getElementById("song_name").innerHTML = "Playing Harry Potter Song";
+        }
+    }
 }
 
 function gotPoses(results) {
@@ -58,6 +74,8 @@ function gotPoses(results) {
         console.log(results);
         
         score_leftWrist = results[0].pose.keypoints[9].score;
+
+        score_rightWrist = results[0].pose.keypoints[10].score;
 
         leftWristX = results[0].pose.leftWrist.x;
         console.log("Left Wrist X= ", leftWristX);
